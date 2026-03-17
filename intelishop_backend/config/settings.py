68fe985228c2773116aadb4c8b13327f98f510af
Eustoma1010 <109512118+Eustoma1010@users.noreply.github.com>
2026-03-17 20:25:19 +0700

    Disable CORS for all origins and specify allowed origin
    Restrict CORS to specified frontend origin.

--- intelishop_backend/config/settings.py
+++ intelishop_backend/config/settings.py
@@ -130,7 +130,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://intelishop-frontend.vercel.app",
+]
 
 
 # Đường dẫn URL để truy cập file (VD: http://localhost:8000/media/file.mp3)
